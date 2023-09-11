db35cacf4a3f6bdd12485ba6909aaae9f7efb3e3
Ameena-Begam <103177166+Ameena-Begam@users.noreply.github.com>
2023-09-11 19:03:10 +0530

    Update main.tf

--- Terraform_module/main.tf
+++ Terraform_module/main.tf
@@ -1,8 +1,9 @@
-
 provider "aws" {
-  region = "ap-south-1" 
-  
+  region = "ap-south-1"
+  access_key = "AKIATLQY4Y2UWZUKN45G"
+  secret_key = "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"
 }
+
 module "vpc" {
   source   = "./vpc"
   vpc_cidr = var.vpc_cidr
